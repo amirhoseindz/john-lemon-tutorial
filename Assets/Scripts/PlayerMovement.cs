236c0a5d6296979e898eff63684d17b2b6e2a9da
unknown <amir.mh17@yahoo.com>
2022-07-21 06:11:07 +0430

    creat a vector for the direction we want our character to face

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 { 
+    public float turnSpeed = 20f;
     Animator m_Animator;
     Vector3 m_Movement;
     // Start is called before the first frame update
@@ -23,5 +24,6 @@ public class PlayerMovement : MonoBehaviour
         bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
         m_Animator.SetBool ("IsWalking", isWalking);
+        Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
     }
 }
