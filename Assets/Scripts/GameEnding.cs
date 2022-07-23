90b7909fbb8783228061c6f73ff4e61852e25869
unknown <amir.mh17@yahoo.com>
2022-07-23 06:51:44 +0430

    edit GameEnding script to separate the way level ends when player get caught or get to exit

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
@@ -8,7 +9,9 @@ public class GameEnding : MonoBehaviour
 	public float displayImageDuration = 1f;
 	public GameObject player;
 	public CanvasGroup exitBackgroundImageCanvasGroup;
+	public CanvasGroup caughtBackgroundImageCanvasGroup;
 	bool m_IsPlayerAtExit;
+ 	bool m_IsPlayerCaught;
 	float m_Timer;
 	void OnTriggerEnter (Collider other)
     {
@@ -17,20 +20,35 @@ public class GameEnding : MonoBehaviour
 			m_IsPlayerAtExit = true;
         }
     }
+	public void CaughtPlayer ()
+    {
+        m_IsPlayerCaught = true;
+    }
 	void Update ()
     {
 		if(m_IsPlayerAtExit)
         {
-			EndLevel ();
+			EndLevel (exitBackgroundImageCanvasGroup, false);
         }
+		else if(m_IsPlayerCaught)
+    	{
+        	EndLevel (caughtBackgroundImageCanvasGroup, true);
+    	}
     }
-	void EndLevel ()
+	void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart)
     {
-		m_Timer += Time.deltaTime;
-		exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
-		if(m_Timer > fadeDuration + displayImageDuration)
+        m_Timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+        if(m_Timer > fadeDuration + displayImageDuration)
         {
-			Application.Quit();
+			if (doRestart)
+			{
+				SceneManager.LoadScene(0);		
+			}
+			else
+			{
+				Application.Quit ();
+			}
         }
     }
 }
